467df542280c9e24b823ea4b045b7d8989bb13e5
Muhamad Alfian Widjaya <alfianmuhammad34@gmail.com>
2022-09-24 10:46:23 +0700

    membuat file serverside

---
+++ src/main/java/co/id/mee/serverside/ServersideApplication.java
@@ -0,0 +1,14 @@
+package co.id.mee.serverside;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServersideApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServersideApplication.class, args);
+                System.out.println("Hello World");
+	}
+
+}
